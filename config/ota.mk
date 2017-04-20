9f1f4e4763dda56833c62b8001a776c6bf790891
dadi11 <dadi.sikorski@gmail.com>
2017-04-20 20:11:39 +0200

    xenonhd: change XenonOTA app
    * build libbypass
    * add new ota build.prop overrides
    * add new definitions
    * create OTA XML file automatically
    * use nil="true" to disable - <CheckMD5 nil="true" />
    * use `export XENONHD_DONATE=http://goo.gl/yourlink` to get donations
    
    Signed-off-by: dadi11 <dadi.sikorski@gmail.com>

--- config/ota.mk
+++ config/ota.mk
@@ -7,22 +7,28 @@ endif
 XENONHD_VERSION := XenonHD-$(shell date +"%y%m%d")-$(OTA_TYPE)
 DEVICE := $(subst xenonhd_,,$(TARGET_PRODUCT))
 
+ifeq ($(XENONHD_DONATE),)
+    XENONHD_DONATE=http://goo.gl/3z5jHW
+endif
+
 # XenonHD OTA app
 PRODUCT_PACKAGES += \
     XenonOTA
 
+# Proprietary libs
+ifneq ($(filter arm64,$(TARGET_ARCH)),)
+PRODUCT_COPY_FILES += \
+    vendor/xenonhd/prebuilt/common/lib/libbypass.so:system/lib/libbypass.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/xenonhd/prebuilt/common/lib64/libbypass.so:system/lib64/libbypass.so
+endif
+
 # Build.prop overrides
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.xenonhd.version=$(XENONHD_VERSION) \
+    ro.xenonhd.donate=$(XENONHD_DONATE) \
+    ro.ota.systemname=XenonHD-$(OTA_TYPE) \
     ro.ota.device=$(DEVICE) \
-    ro.ota.type=$(OTA_TYPE)
-
-$(shell echo -e "# OTA_configuration\n \
-ota_url=https://mirrors.c0urier.net/android/teamhorizon/N/OTA/ota_nougat_$(DEVICE).xml\n \
-ota-device=$(DEVICE)\n \
-device_name=ro.ota.device\n \
-release_type=Stable\n \
-version_source=ro.xenonhd.version\n \
-version_delimiter=-\n \
-version_position=1\n \
-version_format=yyMMdd" > $(ANDROID_BUILD_TOP)/ota_conf)
+    ro.ota.version=$(shell date +"%y%m%d") \
+    ro.ota.manifest=https://mirrors.c0urier.net/android/teamhorizon/N/OTA/ota_nougat_$(DEVICE).xml
